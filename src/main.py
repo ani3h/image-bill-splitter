03c39c84efb1ddf1d966970a22a726c5815111fa
Anish Kantheti <anishkantheti03@gmail.com>
2025-04-20 15:14:14 +0530

    remove main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from src.imageprocessing import extract_text
-from src.billparsing import extract_invoice_data
-
-text = extract_text("bill4.jpeg")
-invoice_data = extract_invoice_data(text)
-
-print(text)
-print(invoice_data)
